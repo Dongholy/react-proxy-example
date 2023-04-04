a8fd6aea8e222981cd5dd555c96ad0153cf0a27a
Dongholy <dongholy@naver.com>
2023-04-04 17:41:36 +0900

    clear

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import BookTable from './components/BookTable';
+import TodoTable from './components/TodoTable';
 import DisplayBoard from './components/DisplayBoard';
 import CreateBook from './components/CreateBook';
-import { getAllBooks, createBook } from './services/BookService';
+import CreateTodo from './components/CreateTodo';
+import { getAllBooks, createBook, createTodos, getAllTodos } from './services/BookService';
 import Footer from './components/Footer';
 
 function App () {
@@ -13,11 +15,22 @@ function App () {
   const [books, setBooks] = useState([]);
   const [numberOfBooks, setNumberBooks] = useState(0);
 
+  const [myTodo, setMyTodo] = useState({});
+  const [todos, setTodos] = useState([]);
+  const [numberOfTodos, setNumberOfTodos] = useState(0);
+
   const handleSubmit = () => {
       createBook(bookShelf)
         .then(() => {
           setNumberBooks(numberOfBooks+1);
       });
+  }
+
+  const handleTodoSubmit = () => {
+    createTodos(myTodo)
+      .then(() => {
+        setNumberOfTodos(numberOfTodos+1);
+    });
   }
 
   const getAllBook = () => {
@@ -27,6 +40,16 @@ function App () {
         setNumberBooks(data.length);
       });
   }
+
+  const getAllTodo = () => {
+    getAllTodos()
+      .then(data => {
+        console.log(data)
+        setTodos(data);
+        setNumberOfTodos(data.length);
+      });
+  }
+
 
   const handleOnChangeForm = (e) => {
       let inputData = bookShelf;
@@ -40,6 +63,18 @@ function App () {
       setBookShelf(inputData);
   }
 
+  const handleOnChangeTodoForm = (e) => {
+    let inputData = myTodo;
+    if (e.target.name === 'todo') {
+      myTodo.todo = e.target.value;
+    } else if (e.target.name === 'category') {
+      myTodo.category = e.target.value;
+    } else if (e.target.name === 'isComplete') {
+      myTodo.isComplete = e.target.value;
+    }
+    setMyTodo(inputData);
+  }
+
   
   return (
     <div className="main-wrapper">
@@ -50,11 +85,21 @@ function App () {
           onChangeForm={handleOnChangeForm}
           handleSubmit={handleSubmit}
         />
+
+        <CreateTodo 
+        myTodo={myTodo}
+        onChangeForm={handleOnChangeTodoForm}
+        handleSubmit={handleTodoSubmit}
+        />
+
         <DisplayBoard 
           numberOfBooks={numberOfBooks} 
           getAllBook={getAllBook} 
+          getAllTodo={getAllTodo} 
+          numberOfTodos={numberOfTodos}
         />
         <BookTable books={books} />
+        <TodoTable todos={todos} />
         <Footer />
       </div>
     </div>
